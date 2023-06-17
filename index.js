c08e3c8f4897ea4294acdc4a97d74c7b92d62f4b
Falgun Jairaj Thakwani <fl700637@dal.ca>
2023-06-17 10:58:15 -0300

    Testing Container 1

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/',(req,res) => {
-  res.json('Container 1 Running...');
+  res.json('Everything is fine ');
 });
 
 // app.get('/',(req,res) => {
